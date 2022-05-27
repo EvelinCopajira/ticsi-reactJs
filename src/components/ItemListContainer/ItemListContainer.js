1c27df8bb0eb7a631d17e3d7de4eeea20c7fe9e1
evelin copajira <evelincopajira@gmail.com>
2022-05-27 01:36:01 -0300

    Simulo info de API, sumo promise y useEffect

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,30 +1,87 @@
 //ItemListContainer.js - componente contenedor (lista todas las cards que se muestran en pantalla)
-//import CardItem
-import CardItem from '../Item/Item';
-import { Grid } from '@mui/material';
+
+//import itemList
+import ItemList from '../ItemList/ItemList';
+
+import { useState, useEffect } from 'react';
 
 //le agreo una prop "title" para poder reutilizarlo y que cada vez que llame a un ItemListContainer le puedo cambiar el nombre y personalizarlo
-const ItemListContainer = ({title}) => {
-    return (
-        <>
-        <h2>{title}</h2>
-        <Grid container>
-            <Grid item md={3}>
-                <CardItem title={'Kokedama 1'} price={500} image={'ticsi-logo.png'}/>
-            </Grid>
-            <Grid item md={3}>
-                <CardItem title='Kokedama 2' price={1500} image={'ticsi-logo.png'}/>
-            </Grid>
-            <Grid item md={3}>
-                <CardItem title='Kokedama 3' price={5500} image={'ticsi-logo.png'}/>
-            </Grid>
-            <Grid item md={3}>
-                <CardItem title='Kokedama 4' price={2500} image={'ticsi-logo.png'}/>
-            </Grid>
-        </Grid>        
-        </>
+const ItemListContainer = () => {
 
+    //setProducts tiene la respuesta de nuestra promise
+    const [products, setProducts] = useState ([]);
+        
+    //simula info que viene de la API
+    const productsTicsi = [
+    {   
+        id: 1,
+        title: 'Kokedama 1',
+        description:'',
+        price: 1500,
+        image: 'ticsi-logo.png',
+        stock: 2
+
+    },
+    {
+        id: 2,
+        title: 'Kokedama 2',
+        description:'',
+        price: 500,
+        image: 'ticsi-logo.png',
+        stock: 3,
+
+    },
+    {
+        id: 3,
+        title: 'Kokedama 3',
+        description:'',
+        price: 2500,
+        image: 'ticsi-logo.png',
+        stock: 10,
+    },
+    {
+        id: 4,
+        title: 'Kokedama 4',
+        description:'',
+        price: 800,
+        image: 'ticsi-logo.png',
+        stock: 1,
+    }
+    ];
+
+    //promise para simular la llamada a la API
+    const getProducts = () => {
+        return new Promise ((resolve, reject) => {
+            
+            //setTimeout para la "espera" de 2 min
+            setTimeout (() => {
+                resolve (productsTicsi)
+            }, 2000)
+        })
+    }
+
+    //useEffect para que llame al componente 1 sola vez, solo cuando es creado
+    useEffect( () => {
+        getProducts()
+        //llamado asincrono
+        .then( (response) => {
+            setProducts (response)
+        }) 
+
+        .catch( (error) => {
+        })
+        
+        .finally( () => {            
+        })        
+    }, [])   
+
+    return (
+        <div>
+            <ItemList title={'PRODUCTOS DESTACADOS'} products={products}></ItemList>
+            <ItemList title={'PRODUCTOS EN OFERTA'} products={products}></ItemList>
+        </div>
     )
+
 }
 
-export default ItemListContainer 
+export default ItemListContainer;
