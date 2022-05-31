4a992f14bfe95387bfece3dd70772aa45918131a
evelin copajira <evelincopajira@gmail.com>
2022-05-31 01:48:09 -0300

    Creo carpeta utils para poder llevar el array de productos

---
+++ src/utils/ProductsMock.js
@@ -0,0 +1,47 @@
+//simula info que viene de la API
+const productsTicsi = [
+    {   
+        id: 1,
+        title: 'Kokedama 1',
+        description:'',
+        price: 1500,
+        image: 'ticsi-logo.png',
+        stock: 2
+    },
+    {
+        id: 2,
+        title: 'Kokedama 2',
+        description:'',
+        price: 500,
+        image: 'ticsi-logo.png',
+        stock: 3
+    },
+    {
+        id: 3,
+        title: 'Kokedama 3',
+        description:'',
+        price: 2500,
+        image: 'ticsi-logo.png',
+        stock: 10
+    },
+    {
+        id: 4,
+        title: 'Kokedama 4',
+        description:'',
+        price: 800,
+        image: 'ticsi-logo.png',
+        stock: 1
+    }
+];
+
+const productTicsi1 = {
+    id: 3,
+    title: 'Kokedama 3',
+    description:'',
+    price: 2500,
+    image: 'ticsi-logo.png',
+    stock: 10
+}
+
+export default productsTicsi;
+export {productTicsi1}
