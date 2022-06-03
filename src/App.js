05e43d198ab582abc76b61ffc476454df14dceae
evelin copajira <evelincopajira@gmail.com>
2022-06-03 15:42:17 -0300

    Se agrega route para category

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Contact from './pages/Contact';
 import Products from './pages/Products';
 import NotFound from './pages/NotFound';
 import Detail from './pages/Detail';
+import ProductsList from './pages/ProductsList';
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/products' element={<Products />}></Route>
+          <Route path='/items/:category' element={<ProductsList/>}></Route>
           <Route path='/item/:id' element={<Detail />}></Route>
           <Route path='/contact' element={<Contact />}></Route>
           <Route path='*' element={<NotFound />}></Route>
