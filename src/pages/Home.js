96949cb720fc89b4c0b48e45d2785bc8d9db0d8e
evelin copajira <evelincopajira@gmail.com>
2022-06-06 23:56:42 -0300

    Agrego title a la sección HOME

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import ItemListContainer from "../components/ItemListContainer/ItemListContainer
 const Home = () => {
     return (
         <div className='general-container'>
+            <h1>HOME</h1>
             <ItemListContainer>
             </ItemListContainer>
         </div>
