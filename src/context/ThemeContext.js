42bc919ed9344608c5db47b9291c3d360f82609b
evelin copajira <evelincopajira@gmail.com>
2022-06-09 01:31:03 -0300

    Agrego carpeta context para implementarlo en la app

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,30 @@
+import { createContext } from "react";
+import { useState } from "react";
+
+//guardo context en una constante
+const ThemeContext = createContext();
+
+const ThemeProvider = ({children}) => {
+    //estado para definir si la página tiene o no que mostrar darkMode
+    const [darkTheme, setDarkTheme] = useState(false)
+
+    //funcionn para transformar el setDarkTheme de false a true     
+    const handleTheme = () => {
+        console.log('se cambio el theme ');
+        setDarkTheme(!darkTheme)
+    }
+
+    const data = {
+        darkTheme,
+        handleTheme
+    }
+
+    return(
+        <ThemeContext.Provider value={data}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeProvider
+export {ThemeContext}
